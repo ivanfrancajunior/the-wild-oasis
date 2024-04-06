eaa00e93b3f614560e00435ed0b90a2c8cb56f3c
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-06 11:19:44 -0300

    feat:add AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,23 @@ import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="/dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/users" element={<Users />} />
+          </Route>
           <Route path="/login" element={<Login />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/users" element={<Users />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>

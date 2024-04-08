4aad468b87995e61c8a29e337ef59964fe927ad6
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-08 18:36:57 -0300

    feat: add useCreateCabin custom hook

---
+++ src/features/cabins/useCreateCabin.js
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createOrEditCabin } from "../../services/apiCabins";
+
+export const useCreateCabin = () => {
+  const queryClient = useQueryClient();
+  const { mutate: createCabin, isLoading: isCreating } = useMutation({
+    mutationFn: createOrEditCabin,
+    onSuccess: () => {
+      toast.success("Cabin sucessfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { createCabin, isCreating };
+};
